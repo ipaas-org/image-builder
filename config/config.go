44ca23741b81ac2fad7b7fddb33a3cf51abd8de9
vano2903 <davidevanoncini2003@gmail.com>
2023-03-16 13:38:19 +0100

    feat: implement builder and downloader config

--- config/config.go
+++ config/config.go
@@ -9,7 +9,6 @@ import (
 type (
 	Config struct {
 		App      `yaml:"app"`
-		HTTP     `yaml:"http"`
 		Log      `yaml:"logger"`
 		Database `yaml:"database"`
 		Services `yaml:"services"`
@@ -18,11 +17,6 @@ type (
 	App struct {
 		Name    string `env-required:"true" yaml:"name"    env:"APP_NAME"`
 		Version string `env-required:"true" yaml:"version" env:"APP_VERSION"`
-	}
-
-	HTTP struct {
-		Port      string `env-required:"true" yaml:"port" env:"HTTP_PORT"`
-		JWTSecret string `env-required:"true" yaml:"jwtSecret" env:"JWT_SECRET"`
 	}
 
 	Log struct {
@@ -36,12 +30,18 @@ type (
 	}
 
 	Services struct {
-		Logo LogoService `yaml:"logo"`
+		Downloaders []Downloader `yaml:"downloaders,flow"`
+		Builders    []Builder    `yaml:"builders,flow"`
 	}
 
-	LogoService struct {
-		BaseUrl string `env-required:"true" yaml:"base_url" env:"BASE_URL"`
-		ApiKey  string `env-required:"true" yaml:"api_key"  env:"API_KEY"`
+	Downloader struct {
+		Name              string `yaml:"name"`
+		DownloadDirectory string `yaml:"downloadDirectory"`
+	}
+
+	Builder struct {
+		Name        string `yaml:"name"`
+		RegistryUri string `yaml:"registryUri"`
 	}
 )
 
