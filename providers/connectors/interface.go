125e1b9af54b213babc7cade5353bb5c5711250a
vano2903 <davidevanoncini2003@gmail.com>
2023-03-30 19:22:41 +0200

    feat: add connector interface

--- providers/connectors/interface.go
+++ providers/connectors/interface.go
@@ -1,8 +1,8 @@
 package connectors
 
 type Connector interface {
-	Pull(userID, branch, url string) (path, repoName, lastCommit string, err error)
-	GetUserAndRepo(url string) (username string, repoName string, err error)
-	ValidateAndLintUrl(url string) (linted string, err error)
-	GetMetadata(url string) (metadata map[string][]string, err error)
+	Pull(userID, branch, url, token string) (path, repoName, lastCommit string, err error)
+	GetUserAndRepo(url, token string) (username string, repoName string, err error)
+	ValidateAndLintUrl(url, token string) (linted string, err error)
+	GetMetadata(url, token string) (metadata map[string][]string, err error)
 }
