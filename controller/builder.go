f6e5f790314bd198a473cc81acbbad159fee55e3
vano2903 <davidevanoncini2003@gmail.com>
2023-04-06 11:09:36 +0200

    feat: add registry to controller and granular meta retrival

--- controller/builder.go
+++ controller/builder.go
@@ -3,23 +3,30 @@ package controller
 import (
 	"context"
 	"errors"
+	"fmt"
+	"os"
+	"strings"
 
+	"github.com/ipaas-org/image-builder/model"
+	"github.com/ipaas-org/image-builder/providers/builders"
+	"github.com/ipaas-org/image-builder/providers/connectors"
+	"github.com/ipaas-org/image-builder/providers/registry"
 	"github.com/sirupsen/logrus"
-	"github.com/vano2903/image-builder/model"
-	"github.com/vano2903/image-builder/providers/builders"
-	"github.com/vano2903/image-builder/providers/connectors"
 )
 
-// var _ BuilderController = new(Builder)
+var _ BuilderController = new(Builder)
 
 var (
 	ErrConnectorNotFound = errors.New("connector not found")
 	ErrBuilderNotFound   = errors.New("builder not found")
+	ErrEmptyToken        = errors.New("empty token")
+	ErrInvalidToken      = errors.New("invalid token")
 )
 
 type Builder struct {
 	connectors map[string]connectors.Connector
 	builders   map[string]builders.Builder
+	registry   registry.Registryer
 	l          *logrus.Logger
 }
 
@@ -31,70 +38,146 @@ func NewBuilderController(log *logrus.Logger) *Builder {
 	}
 }
 
-func (c *Builder) GetConnector(name string) (connectors.Connector, error) {
-	connector, ok := c.connectors[name]
+func (b *Builder) GetConnector(name string) (connectors.Connector, error) {
+	connector, ok := b.connectors[name]
 	if !ok {
 		return nil, ErrConnectorNotFound
 	}
 	return connector, nil
 }
 
-func (c *Builder) GetBuilder(name string) (builders.Builder, error) {
-	builder, ok := c.builders[name]
+func (b *Builder) GetBuilder(name string) (builders.Builder, error) {
+	builder, ok := b.builders[name]
 	if !ok {
 		return nil, ErrConnectorNotFound
 	}
 	return builder, nil
 }
 
-func (c *Builder) AddConnector(name string, conn connectors.Connector) {
-	c.connectors[name] = conn
+func (b *Builder) AddConnector(name string, conn connectors.Connector) {
+	b.connectors[name] = conn
 }
 
-func (c *Builder) AddBuilder(name string, builder builders.Builder) {
-	c.builders[name] = builder
+func (b *Builder) AddBuilder(name string, builder builders.Builder) {
+	b.builders[name] = builder
 }
 
-func (c *Builder) PullRepo(info model.ImageBuildInfo) (model.PulledRepoInfo, error) {
-	connector, ok := c.connectors[info.Connector]
+func (b *Builder) AddRegistry(reg registry.Registryer) {
+	b.registry = reg
+}
+
+func (b *Builder) PullRepo(info model.ImageBuildInfo) (model.PulledRepoInfo, error) {
+	if info.Token == "" {
+		return model.PulledRepoInfo{}, ErrEmptyToken
+	}
+
+	connector, ok := b.connectors[info.Connector]
 	if !ok {
 		return model.PulledRepoInfo{}, ErrConnectorNotFound
 	}
 
-	c.l.Infof("%s is pulling %s at branch %s", info.UserID, info.Repo, info.Branch)
-	c.l.Debugf("info received: %+v", info)
+	b.l.Infof("%s is pulling %s at branch %s", info.UserID, info.Repo, info.Branch)
+	b.l.Debugf("info received: %+v", info)
 
 	var pullInfo model.PulledRepoInfo
 	var err error
-	pullInfo.Path, pullInfo.RepoName, pullInfo.LastCommit, err = connector.Pull(info.UserID, info.Branch, info.Repo)
+	pullInfo.Path, pullInfo.RepoName, pullInfo.LastCommit, err = connector.Pull(info.UserID, info.Branch, info.Repo, info.Token)
 	if err != nil {
-		c.l.Errorf("error pulling %s: %v", info.Repo, err)
-		return model.PulledRepoInfo{}, err
-	}
-
-	pullInfo.Metadata, err = connector.GetMetadata(info.Repo)
-	if err != nil {
-		c.l.Errorf("error getting metadata for %s: %v", info.Repo, err)
+		b.l.Errorf("error pulling %s: %v", info.Repo, err)
 		return model.PulledRepoInfo{}, err
 	}
 
 	return pullInfo, nil
 }
 
-func (c *Builder) BuildImage(path string) (string, string, error) {
-	c.l.Infof("building image from %s", path)
-	builder, ok := c.builders[model.DownloaderNixpacks]
+func (b *Builder) GetMetadata(info model.ImageBuildInfo) (map[connectors.MetaType][]string, error) {
+	if info.Token == "" {
+		return nil, ErrEmptyToken
+	}
+
+	connector, ok := b.connectors[info.Connector]
+	if !ok {
+		return nil, ErrConnectorNotFound
+	}
+
+	b.l.Infof("getting metadata for %s-%s", info.Repo, info.Branch)
+	b.l.Debugf("info received: %+v", info)
+
+	metadata, err := connector.GetMetadata(info.Repo, info.Token)
+	if err != nil {
+		b.l.Errorf("error getting metadata for %s: %v", info.Repo, err)
+		return nil, err
+	}
+	return metadata, nil
+}
+
+func (b *Builder) GetGranularMetadata(info model.ImageBuildInfo, meta ...connectors.MetaType) (map[connectors.MetaType][]string, error) {
+	if info.Token == "" {
+		return nil, ErrEmptyToken
+	}
+
+	connector, ok := b.connectors[info.Connector]
+	if !ok {
+		return nil, ErrConnectorNotFound
+	}
+
+	b.l.Infof("getting %v metadata for %s-%s", meta, info.Repo, info.Branch)
+	b.l.Debugf("info received: %+v", info)
+
+	metadata, err := connector.GetMetadata(info.Repo, info.Token, meta...)
+	if err != nil {
+		b.l.Errorf("error getting metadata for %s: %v", info.Repo, err)
+		return nil, err
+	}
+	return metadata, nil
+}
+
+func (b *Builder) BuildImage(ctx context.Context, info model.ImageBuildInfo, path string) (string, string, error) {
+	//clean up the path
+	defer func() {
+		b.l.Infof("cleaning up %s", path)
+		os.RemoveAll(path)
+	}()
+
+	b.l.Infof("building image from %s", path)
+	builder, ok := b.builders[model.DownloaderNixpacks]
 	if !ok {
 		return "", "", ErrBuilderNotFound
 	}
 
-	buildPlan, err := builder.Plan(context.Background(), path)
+	b.l.Debug("planning build")
+	buildPlan, err := builder.Plan(ctx, path)
 	if err != nil {
-		c.l.Errorf("error planning build: %v", err)
+		b.l.Errorf("error planning build: %v", err)
 		return "", "", err
 	}
 
-	c.l.Debugf("build plan: %+v", buildPlan)
+	b.l.Debugf("build plan: %+v", buildPlan)
+	b.l.Debug("building image")
+	imageID, errorMessage, err := builder.Build(ctx, info.UserID, info.Repo, buildPlan, path)
+	if err != nil {
+		b.l.Errorf("error building image: %v", err)
+		b.l.Errorf("error message: %s", errorMessage)
+		return "", errorMessage, err
+	}
+	return imageID, "", nil
+}
 
-	return builder.Build(context.Background(), buildPlan, path)
+func (b *Builder) GenerateImageName(userID string, info model.PulledRepoInfo) string {
+	repo := strings.Split(info.Path, "/")[len(strings.Split(info.Path, "/"))-1]
+	return fmt.Sprintf("%s/%s:%s", userID, repo, info.LastCommit)
+}
+
+func (b *Builder) PushImage(ctx context.Context, imageID, newImageName string) error {
+	b.l.Infof("pushing image %s as %s", imageID, newImageName)
+	toPush, err := b.registry.TagImage(imageID, newImageName)
+	if err != nil {
+		b.l.Errorf("error tagging image %s as %s: %v", imageID, newImageName, err)
+	}
+
+	if err := b.registry.PushImage(ctx, toPush); err != nil {
+		b.l.Errorf("error pushing image %s: %v", toPush, err)
+		return err
+	}
+	return nil
 }
--- controller/interface.go
+++ controller/interface.go
@@ -1,19 +1,22 @@
 package controller
 
 import (
-	"github.com/vano2903/image-builder/model"
-	"github.com/vano2903/image-builder/providers/builders"
-	"github.com/vano2903/image-builder/providers/connectors"
+	"context"
+
+	"github.com/ipaas-org/image-builder/model"
+	"github.com/ipaas-org/image-builder/providers/builders"
+	"github.com/ipaas-org/image-builder/providers/connectors"
+	"github.com/ipaas-org/image-builder/providers/registry"
 )
 
 type (
 	BuilderController interface {
 		AddBuilder(name string, builder builders.Builder)
 		AddConnector(name string, conn connectors.Connector)
+		AddRegistry(reg registry.Registryer)
 		PullRepo(model.ImageBuildInfo) (model.PulledRepoInfo, error)
-		BuildImage(path string) (imageID string, buildError string, err error)
-		PushImage(imageID string) error
-		CallContainerManager(imageID string) error
-		Clean(path string) error
+		BuildImage(ctx context.Context, info model.ImageBuildInfo, path string) (imageID string, buildError string, err error)
+		PushImage(ctx context.Context, imageID, newImageName string) error
+		// CallContainerManager(imageID string) error
 	}
 )
