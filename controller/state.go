99ce3a103f9533c24f30931657f1912f16cb6038
vano2903 <davidevanoncini2003@gmail.com>
2023-12-08 18:00:50 +0100

    feat: add a state of the application check to know if it should be build or ignore

---
+++ controller/state.go
@@ -0,0 +1,41 @@
+package controller
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/ipaas-org/image-builder/repo"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+func (c *Controller) ShouldBuild(ctx context.Context, applicationID string) (bool, error) {
+	c.l.Println("ShouldBuild")
+	c.l.Printf("applicattionID: %q\n", applicationID)
+	appID, err := primitive.ObjectIDFromHex(applicationID)
+	if err != nil {
+		fmt.Println("invalid applicationID")
+		return false, err
+	}
+	
+	state, err := c.ApplicationRepo.GetStateByID(ctx, appID)
+	if err != nil {
+		if err == repo.ErrNotFound {
+			return false, nil
+		} else {
+			return false, err
+		}
+	}
+	if state == "deleting" {
+		return false, nil
+	}
+	return true, nil
+}
+
+func (c *Controller) UpdateApplicationStateToBuilding(ctx context.Context, applicationID string) error {
+	appID, err := primitive.ObjectIDFromHex(applicationID)
+	if err != nil {
+		return err
+	}
+	_, err = c.ApplicationRepo.UpdateStateByID(ctx, "building", appID)
+	return err
+}
