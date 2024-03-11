55ab4aed16bf4250c6f42c3f3b3d1df342580a90
vano2903 <davidevanoncini2003@gmail.com>
2024-03-11 11:00:06 +0100

    feat: add context in image tagging

--- providers/registry/interface.go
+++ providers/registry/interface.go
@@ -3,6 +3,6 @@ package registry
 import "context"
 
 type Registryer interface {
-	TagImage(localImageID, newName string) (string, error)
+	TagImage(ctx context.Context, localImageID, userCode, appName string) (string, error)
 	PushImage(ctx context.Context, localImageID string) error
 }
