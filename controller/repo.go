374f5b5de8b1bbb3f8065a51c8b5b21061985d2c
vano2903 <davidevanoncini2003@gmail.com>
2024-10-08 13:33:20 +0200

    refactor: add context to connector and use proper return type

--- controller/repo.go
+++ controller/repo.go
@@ -1,8 +1,12 @@
 package controller
 
-import "github.com/ipaas-org/image-builder/model"
+import (
+	"context"
 
-func (b *Controller) PullRepo(info *model.BuildRequest) (*model.PulledRepoInfo, error) {
+	"github.com/ipaas-org/image-builder/model"
+)
+
+func (b *Controller) PullRepo(ctx context.Context, info *model.PullInfoRequest) (*model.PulledRepoInfo, error) {
 	if info.Token == "" {
 		return nil, ErrEmptyToken
 	}
@@ -15,9 +19,7 @@ func (b *Controller) PullRepo(info *model.BuildRequest) (*model.PulledRepoInfo, 
 	b.l.Debugf("info received: %+v", info)
 	b.l.Infof("%s is pulling %s at branch %s", info.UserID, info.Repo, info.Branch)
 
-	pullInfo := new(model.PulledRepoInfo)
-	var err error
-	pullInfo.Path, pullInfo.RepoName, pullInfo.LastCommit, err = connector.Pull(info.UserID, info.Branch, info.Repo, info.Token)
+	pullInfo, err := connector.Pull(ctx, info.UserID, info.Branch, info.Repo, info.Commit, info.Token)
 	if err != nil {
 		b.l.Errorf("error pulling %s: %v", info.Repo, err)
 		return nil, err
